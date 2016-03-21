3f2a211b3eadd5f73f87b6f1821860c78ee817a7
mjmalek218 <mjmalek218@gmail.com>
2016-03-21 18:10:52 -0500

    not much

--- Chap3/inprogress_problems/Three_60.c
+++ Chap3/inprogress_problems/Three_60.c
@@ -3,13 +3,20 @@ x \in %rdi, n \in %esi
 
 loop:
   movl    %esi, %ecx       # lower four bytes of %ecx = n, rest = 0  
-  movl    $1, %edx         # lower four bytes of %edx = 1, rest = 0
-  jmp     .L2              # 
+  movl    $1, %edx         # lower four bytes of %rdx = 1, rest = 0
+  movl    $0, %eax         # %rax = all 0's 
+  jmp     .L2              # move to the L2 loop
 
 .L3:
-  movq    %rdi, %r8        #
-  andq    %rdx, %r8
-  orq     %r8
+  movq    %rdi, %r8        # x in %r8
+  andq    %rdx, %r8        # upper 4 bytes of %rd8 = 0, lower four are the same 
+  orq     %r8, %rax        #          
+  salq    %cl, %rdx        # 
+
+.L2:
+  testq   %rdx, %rdx       # OBVIOUSLY TYPO...LOOK INTO THIS
+  jne     .L3
+  rep; ret
 
 */
 
@@ -28,3 +35,21 @@ long loop(long x, long n)
 
 
 }
+
+
+/*
+
+A. 
+
+B. 
+
+C. 
+
+D.
+
+E.
+
+F.
+
+
+ */
